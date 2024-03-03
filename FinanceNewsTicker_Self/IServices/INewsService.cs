7f5ce5bf88d4bef6def95ae11694920888085cf1
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-03 18:19:06 +0500

    API_URL added in appsettings.json; get data in GetFinanceNews() method in 'NewsService'

--- FinanceNewsTicker_Self/IServices/INewsService.cs
+++ FinanceNewsTicker_Self/IServices/INewsService.cs
@@ -17,6 +17,19 @@
         public void GetFinanceNews()
         {
             string apiKey = _configuration.GetValue<string>("API_KEY");
+            string baseUrl = _configuration.GetValue<string>("API_URL");
+
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+
+                var response = client.GetAsync("?apikey=" + apiKey).Result;
+
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                }
+            }
         }
     }
 }
