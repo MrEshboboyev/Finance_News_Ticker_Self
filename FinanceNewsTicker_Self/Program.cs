091352d7c9c235d31432e708a2704fb43027222c
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-03 18:20:08 +0500

    AddScoped for Lifetime : INewsService, NewsService

--- FinanceNewsTicker_Self/Program.cs
+++ FinanceNewsTicker_Self/Program.cs
@@ -1,7 +1,10 @@
+using FinanceNewsTicker_Self.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<INewsService, NewsService>();
 
 var app = builder.Build();
 
