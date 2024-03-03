7e2455b6c4572fc11ef8bda6e0273ce4180e260e
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-03 18:24:59 +0500

    GetFinanceNews() action in Index.cshtml.cs; Install NewtonSoft package

--- FinanceNewsTicker_Self/Pages/Index.cshtml.cs
+++ FinanceNewsTicker_Self/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using FinanceNewsTicker_Self.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +7,18 @@ namespace FinanceNewsTicker_Self.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly INewsService _newsService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            INewsService newsService)
         {
             _logger = logger;
+            _newsService = newsService;
         }
 
         public void OnGet()
         {
-
+            _newsService.GetFinanceNews();
         }
     }
 }
