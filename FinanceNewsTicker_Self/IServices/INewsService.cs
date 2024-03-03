da3d085805e303bf2cb20e07793b16b72d2e70fb
MrEshboboyev <mreshboboyev@gmail.com>
2024-03-03 18:13:15 +0500

    'NewsService' class in 'Services/INewsService.cs' (variable apiKey); Geting API_KEY from account apiLayer.com;

--- FinanceNewsTicker_Self/IServices/INewsService.cs
+++ FinanceNewsTicker_Self/IServices/INewsService.cs
@@ -2,6 +2,21 @@
 {
     public interface INewsService
     {
-        void GerFinanceNews();
+        void GetFinanceNews();
+    }
+
+    public class NewsService : INewsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public NewsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void GetFinanceNews()
+        {
+            string apiKey = _configuration.GetValue<string>("API_KEY");
+        }
     }
 }
